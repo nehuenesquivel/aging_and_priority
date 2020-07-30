b46f0769fc3f87ae0855d2244b3994baf9b56ed7
nehuenesquivel <nehuenesquivel@outlook.com>
2020-07-30 14:41:08 -0300

    file name input

--- aging_and_priority.py
+++ aging_and_priority.py
@@ -2,18 +2,19 @@ def aging(line):
     return line.replace(line[1:3], str(int(line[1:3]) + 1).zfill(2))
 
 def read_file_and_apply_aging():
-    with open("file.txt", mode="r", encoding="utf-8") as file:
+    with open(file_name + ".txt", mode="r", encoding="utf-8") as file:
         for line in file:
             lines.append(aging(line.strip()))
     file.close()
 
 def apply_priority_and_write_file():
     lines.sort(reverse = True)
-    with open("file.txt", mode="w", encoding="utf-8") as file:
+    with open(file_name + ".txt", mode="w", encoding="utf-8") as file:
         for line in lines:
             file.write(line + "\n")
     file.close()
 
 lines = []
+file_name = input()
 read_file_and_apply_aging()
 apply_priority_and_write_file()
